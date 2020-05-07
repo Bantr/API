a78fd0f1710f7a3bfafd6572f1c75ccc49f3210a
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-05-07 22:12:12 +0200

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,19 +9,21 @@ import { NotificationsModule } from './notifications/notifications.module';
 import { UserSettingsModule } from './user-settings/user-settings.module';
 import { UserModule } from './user/user.module';
 
-// Does not happen in config service because it's a PITA to parse the values from the service inside module initialization
 dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      // TODO: Handle this config via config service
+      type: "postgres",
       host: process.env.BANTR_PG_HOST,
       port: parseInt(process.env.BANTR_PG_PORT, 10),
       username: process.env.BANTR_PG_USER,
       password: process.env.BANTR_PG_PW,
       database: process.env.BANTR_PG_DB,
-      entities: [__dirname + "/../node_modules/@bantr/lib/**/*.entity{.ts,.js}"],
+      entities: [
+        __dirname + "/../node_modules/@bantr/lib/**/*.entity{.ts,.js}"
+      ],
       synchronize: false
     }),
     AuthModule,
@@ -32,4 +34,4 @@ dotenv.config();
   controllers: [AppController],
   providers: [ConfigService]
 })
-export class AppModule { }
+export class AppModule {}
