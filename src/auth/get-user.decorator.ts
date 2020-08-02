70171cb3d78517b3ef8b2a7369ff06adb5e3a8cd
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-08-02 16:06:22 +0200

    Fix incorrect parsing of boolean string in getUser decorator

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -5,18 +5,18 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
  * Custom decorator to get User from a request
  */
 export const GetUser = createParamDecorator(
-    (data: string, ctx: ExecutionContext) => {
+  (data: string, ctx: ExecutionContext) => {
+    const isTest = process.env.BANTR_IS_TEST === "true";
 
-        const isTest = process.env.BANTR_IS_TEST;
+    if (isTest) {
+      const user = new User();
+      user.id = 1;
+      return user;
+    }
 
-        if (isTest) {
-            const user = new User();
-            user.id = 1;
-            return user;
-        }
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.session.passport.user;
 
-        const request = ctx.switchToHttp().getRequest();
-        const user = request.session.passport.user;
-
-        return data ? user && user[data] : user;
-    });
+    return data ? user && user[data] : user;
+  }
+);
