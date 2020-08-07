072372d8b2a258ed41e6e603268ca1a08faa6f74
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-08-07 16:11:00 +0200

    Fix tests

--- src/user-settings/user-settings.service.spec.ts
+++ src/user-settings/user-settings.service.spec.ts
@@ -1,25 +1,37 @@
+import { HttpService } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { UserSettingsRepository } from './user-settings.repository';
 import { UserSettingsService } from './user-settings.service';
 
-describe('UserSettingsService', () => {
+describe("UserSettingsService", () => {
   let service: UserSettingsService;
 
-  const mockUserSettingsRepository = () => ({
-  });
+  const mockUserSettingsRepository = () => ({});
+
+  const mockHttpService = {};
+  const mockConfigService = {
+    get: jest.fn()
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserSettingsService,
-        { provide: UserSettingsRepository, useFactory: mockUserSettingsRepository }
+      providers: [
+        UserSettingsService,
+        {
+          provide: UserSettingsRepository,
+          useFactory: mockUserSettingsRepository
+        },
+        { provide: HttpService, useValue: mockHttpService },
+        { provide: ConfigService, useValue: mockConfigService }
       ]
     }).compile();
 
     service = module.get<UserSettingsService>(UserSettingsService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
